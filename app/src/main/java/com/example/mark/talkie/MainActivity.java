94002fd3c1cd8a5ed5bd370543df6a90df9df4ac
Mark Andrew <mark.andrew@gmail.com>
2016-07-09 23:30:44 +0200

    start of button implementation

--- app/src/main/java/com/example/mark/talkie/MainActivity.java
+++ app/src/main/java/com/example/mark/talkie/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.mark.talkie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupInterface();
+    }
+
+    private void setupInterface() {
+
+        Button sendButton = (Button) findViewById(R.id.sendButton);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText spokenText = (EditText) findViewById(R.id.requestText);
+                // read contents of text field and send to socket
+                String foo = spokenText.getText().toString();
+                final int foo1 = Log.d("FOO", foo);
+            }
+        });
     }
 }
