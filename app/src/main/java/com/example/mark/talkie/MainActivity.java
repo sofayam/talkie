4f6b76b462f53d35a75c39f3296c1187acd9ce5e
Mark Andrew <mark.andrew@gmail.com>
2016-07-10 08:44:59 +0200

    STT and TTS closed circle

--- app/src/main/java/com/example/mark/talkie/MainActivity.java
+++ app/src/main/java/com/example/mark/talkie/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mark.talkie;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,10 +15,11 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
 
     private  EditText spokenText ;
     private final int REQ_CODE_SPEECH_INPUT = 100;
+    private TextToSpeech tts ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,12 +28,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setupInterface() {
-
+        spokenText = (EditText) findViewById(R.id.requestText);
         Button sendButton = (Button) findViewById(R.id.sendButton);
+        Button readbackButton = (Button) findViewById(R.id.readbackButton);
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                spokenText = (EditText) findViewById(R.id.requestText);
+
                 // read contents of text field and send to socket
                 String foo = spokenText.getText().toString();
                 final int foo1 = Log.d("FOO", foo);
@@ -45,6 +48,20 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+        readbackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                readBack();
+            }
+
+        });
+        tts = new TextToSpeech(this,this);
+    }
+
+    private void readBack() {
+        tts.setLanguage(Locale.getDefault());
+        String spoken = spokenText.getText().toString();
+        tts.speak(spoken, TextToSpeech.QUEUE_ADD, null);
     }
 
     private void promptSpeechInput() {
@@ -81,5 +98,18 @@ public class MainActivity extends AppCompatActivity {
             }
 
         }
+    }
+
+    @Override
+    public void onInit(int status) {
+        if(status == TextToSpeech.SUCCESS){
+            int result=tts.setLanguage(Locale.getDefault());
+            if(result==TextToSpeech.LANG_MISSING_DATA ||
+                    result==TextToSpeech.LANG_NOT_SUPPORTED){
+                Log.e("error", "This Language is not supported");
+            }
+        }
+        else
+            Log.e("error", "Initialization Failed!");
     }
 }
